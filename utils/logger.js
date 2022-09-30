cec449fd000ad31c9127eb426ffc52a2d45cd693
Marco <acc@save-bot.eu>
2022-09-30 16:01:34 +0200

    Begin of logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,39 @@
 'use strict';
+const fs = require("fs")
+
 class Logger{
+    isDebug = false;
+    loggerId = false;
 
-    constructor(name){
-        this.name = name ;
+    constructor(){
+
+        fs.open("./last_log.log", "w", function(err, file) { if(err) throw err})
+
+        if(!fs.existsSync("./logs")) {
+
+            fs.mkdirSync("./logs")
+
+        }
+
     }
 
-    print(){
-        console.log('Name is :'+ this.name);
+    writeLog(loggerId, message) {
+
+        fs.appendFileSync("./last_log.log", message, function(err) {if (err) throw err})
+        fs.appendFileSync(`./logs/${this.loggerId}`)
+
     }
+
+    setLoggerId(loggerId) {
+
+        this.loggerId = loggerId
+
+    }
+
+    getLoggerId() {
+
+        return this.loggerId
+
+    }
+
 }
