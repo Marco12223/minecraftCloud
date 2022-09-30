7477baf0ad65e9e8060e77d0b62d81c5bb48faf8
Marco <acc@save-bot.eu>
2022-09-30 16:26:51 +0200

    Finish first version of the logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,15 @@
 'use strict';
 const fs = require("fs")
+const colours = require("./colours");
 
-class Logger{
+class getLogger{
     isDebug = false;
     loggerId = false;
+    currDate = null;
 
     constructor(){
+
+        this.currDate = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
 
         fs.open("./last_log.log", "w", function(err, file) { if(err) throw err})
 
@@ -20,7 +24,7 @@ class Logger{
     writeLog(loggerId, message) {
 
         fs.appendFileSync("./last_log.log", message, function(err) {if (err) throw err})
-        fs.appendFileSync(`./logs/${this.loggerId}`)
+        fs.appendFileSync(`./logs/${this.loggerId}.log`, message, function (err) {if(err) throw err})
 
     }
 
@@ -36,4 +40,36 @@ class Logger{
 
     }
 
+    alert(message) {
+
+        this.writeLog(this.loggerId, message)
+        console.log(`${colours.fg.black}[${colours.fg.yellow}${this.currDate}${colours.fg.black}]${colours.reset} ${colours.fg.black}[${colours.fg.red}ALERT${colours.fg.black}] ${colours.fg.red}${message}${colours.reset}`)
+
+    }
+
+    info(message) {
+
+        this.writeLog(this.loggerId, message)
+        console.log(`${colours.fg.black}[${colours.fg.yellow}${this.currDate}${colours.fg.black}]${colours.reset} ${colours.fg.black}[${colours.fg.blue}INFO${colours.fg.black}] ${colours.fg.green}${message}${colours.reset}`)
+
+    }
+
+    warning(message) {
+
+        this.writeLog(this.loggerId, message)
+        console.log(`${colours.fg.black}[${colours.fg.yellow}${this.currDate}${colours.fg.black}]${colours.reset} ${colours.fg.black}[${colours.fg.yellow}WARNING${colours.fg.black}] ${colours.fg.yellow}${message}${colours.reset}`)
+
+    }
+
+    debug(message) {
+
+        this.writeLog(this.loggerId, message)
+        if(this.isDebug) {
+            console.log(`${colours.fg.black}[${colours.fg.yellow}${this.currDate}${colours.fg.black}]${colours.reset} ${colours.fg.black}[${colours.fg.blue}DEBUG${colours.fg.black}] ${colours.fg.white}${message}${colours.reset}`)
+        }
+
+    }
+
 }
+
+module.exports = {getLogger}
